d3055ceef9235dd35da1250bb16f177a7b542f36
高原 <yuan.gao03@hand-china.com>
2019-07-12 15:21:53 +0800

    init

--- 京东/jd-h5/js/index-wodedingdan.js
+++
@@ -1,7 +0,0 @@
-document.documentElement.style.fontSize = window.innerWidth / 3.75 + 'px';
-$(function(){
-	$(".nav-wrap>li").click(function(){			
-		$(this).siblings().removeClass("nav-1");
-		$(this).addClass("nav-1");
-	})
-});
